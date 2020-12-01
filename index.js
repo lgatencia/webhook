ae2c987292335019f16282ef51cff3141115dcf3
lgatencia <36711071+lgatencia@users.noreply.github.com>
2020-11-30 22:39:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,94 @@
-"use strict";
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+ 
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const mysql = require ('mysql'); 							
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
 
-const express = require("express");
-const bodyParser = require("body-parser");
+  // // Uncomment and edit to make your own intent handler
+  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://assistant.google.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
 
-const restService = express();
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
+  // // See https://github.com/dialogflow/fulfillment-actions-library-nodejs
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
 
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-
-restService.use(bodyParser.json());
-
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again."+req.body;
-  return res.json({
-
-  "fulfillmentText": speech,
-  "fulfillmentMessages": [
-    {
-      "text": {
-        "text": [speech]
-      }
-    }
-  ],
-  "source": "<webhookpn1>"
-
-
-  });
-});
-
-
-restService.listen(process.env.PORT || 8000, function() {
-  console.log("Server up and listening");
+  // Run the proper function handler based on the matched Dialogflow intent name
+  function connectToDatabase(){
+    const connection = mysql.createConnection({
+      host     : '213.190.6.11',
+      user     : 'u889550350_LuisGomez',
+      password : '456123luiS@',
+      database : 'u889550350_Roboton'
+    });
+    return new Promise((resolve,reject) => {
+       connection.connect();
+       resolve(connection);
+    });
+  }
+  function queryDatabase(connection){
+    return new Promise((resolve, reject) => {
+      connection.query('SELECT * from Clientes', (error, results, fields) => {
+        resolve(results);
+      });
+    });
+  }
+  
+   function handleReadFromMySQL(agent){
+    return connectToDatabase()
+    .then(connection => {
+      return queryDatabase(connection)
+      .then(result => {
+        console.log(result);
+        agent.add(`Nombre: ${result[0].Nombre} and Genero: ${result[0].Genero}`);
+        connection.end();
+      });
+    });
+  }
+  
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('DataMysql', handleReadFromMySQL);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
 });
